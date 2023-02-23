89b5b0acc83a6917b590e06830830eb3d9213a85
pedro <pedrovianaasking@gmail.com>
2023-02-23 00:52:49 -0300

    [Mod] Módulos criados

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { ArticleModule } from './article/article.module';
+import { SocialModule } from './social/social.module';
 @Module({
-  imports: [UserModule, PrismaClient, AuthModule],
+  imports: [UserModule, PrismaClient, AuthModule, ArticleModule, SocialModule],
   controllers: [],
   providers: [],
 })
